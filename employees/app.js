f1a8098f2546168dff9e07fa3d7d5246a113d167
Kingsimeone <simeoneumoh@outlook.com>
2020-12-09 11:39:47 +0100

    import multiple items form external module

--- employees/app.js
+++ employees/app.js
@@ -1,3 +1,5 @@
 const tutorial = require('./tutorial')
-console.log(tutorial);
-console.log(tutorial(7,3));
+
+console.log(tutorial.sum(7,3));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject());
